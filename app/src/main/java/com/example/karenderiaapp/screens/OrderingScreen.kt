fca295f1b9a66a55c8f480979a7806bb8fb4859a
Eligsir <eligsircodes@gmail.com>
2026-02-02 18:22:27 +0800

    Full Commit

--- app/src/main/java/com/example/karenderiaapp/screens/OrderingScreen.kt
+++ app/src/main/java/com/example/karenderiaapp/screens/OrderingScreen.kt
@@ -1,13 +1,27 @@
 package com.example.karenderiaapp.screens
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
 import com.example.karenderiaapp.data.FakeData
 import com.example.karenderiaapp.data.Order
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrderingScreen(onNavigateSummary: () -> Unit) {
     val orders = remember { mutableStateListOf<Order>() }
-
 
     Scaffold(
         topBar = { TopAppBar(title = { Text("Ordering") }) }
@@ -31,7 +45,6 @@ fun OrderingScreen(onNavigateSummary: () -> Unit) {
                     }
                 }
             }
-
 
             Button(
                 onClick = onNavigateSummary,
